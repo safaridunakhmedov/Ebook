1d37684734c3672d4817cd6bbc5887a433700c8f
user <faridun.ahmedov.97@mail.ru>
2019-09-30 11:56:01 +0300

    20190930_11_55

---
+++ works/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// needs for printf to work
+
+void main()
+{
+// Hello world; // absolutely wrong
+// "Hello world"; // working, but text doesn't apper on screen
+ printf("Hello world \n");
+}
---
+++ works/mycode.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+ int main() {
+  /* Comment */
+  printf("Why have i created this? \n");
+
+  return 0;
+}
+
