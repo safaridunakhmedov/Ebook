b5667ade80f1900ef1f11116c03368f69b5b3bd9
user <faridun.ahmedov.97@mail.ru>
2019-11-04 14:07:07 +0200

    2019114_14_06

---
+++ works/operations.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+int a, b, c, d, e;
+
+a = 50;
+b = 60;
+
+c = a + b;
+printf("the result of a+b = %d \n", c);
+
+d = a - b;
+printf("the result of a-b = %d \n", d);
+
+e = a % b;
+printf("the result of e  = %d \n", e);
+
+float f = a / b;
+printf("the result of a/b = %1.1f \n", f);
+
+return 0;
+
+}
