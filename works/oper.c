96e99572952343db7fd3e684503afe334e726e6a
user <faridun.ahmedov.97@mail.ru>
2019-11-11 12:02:08 +0200

    20191111_11_58

---
+++ works/oper.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void main(){
+int a=8, b=9;
+
+printf("%d + %d = %d  (sizeof \"+\" %lu byte(s))\n",a,b,a+b,sizeof(a+b));
+printf("%d %c %d = %d  (sizeof \"%c\" %lu byte(s))\n",a,'%',b,a%b,'%',sizeof(a%b));
+printf("%d * %d = %d  (sizeof \"*\" %lu byte(s))\n",a,b,a*b,sizeof(a*b));
+printf("%d - %d = %d  (sizeof \"-\" %lu byte(s))\n",a,b,a-b,sizeof(a-b));
+printf("%d / %d = %.1f (result of using (float)a/b)\n",a,b,(float)a/b);
+
+printf("%d < %d = %d  (sizeof \"<\" %lu byte(s))\n",a,b,a<b,sizeof(a<b));
+printf("%d > %d = %d  (sizeof \">\" %lu byte(s))\n",a,b,a>b,sizeof(a>b));
+printf("%d <= %d = %d  (sizeof \"<=\" %lu byte(s))\n",a,b,a<=b,sizeof(a<=b));
+printf("%d >= %d = %d  (sizeof \">=\" %lu byte(s))\n",a,b,a>=b,sizeof(a>=b));
+
+printf("%d << %d = %d  (sizeof \"<<\" %lu byte(s))\n",a,b,a<<b,sizeof(a<<b));
+printf("%d >> %d = %d  (sizeof \">>\" %lu byte(s))\n",234,2,234>>2,sizeof(234>>2));
+
+printf("%d += %d = %d  (sizeof \"+=\" %lu byte(s))\n",a,b,a+=b,sizeof(a+=b));
+printf("%d -= %d = %d  (sizeof \"-=\" %lu byte(s))\n",a,b,a-=b,sizeof(a-=b));
+printf("%d *= %d = %d  (sizeof \"*=\" %lu byte(s))\n",a,b,a*=b,sizeof(a*=b));
+printf("%d <<= %d = %d  (sizeof \"<<=\" %lu byte(s))\n",a,b,a<<=b,sizeof(a<<=b));
+printf("%d >>= %d = %d  (sizeof \">>=\" %lu byte(s))\n",a,b,a>>=b,sizeof(a>>=b));
+
+printf("%d++ = %d (size of \"a++\" %lu byte(s))\n",a,a++,sizeof(a++));
+printf("++%d = %d (size of \"++a\" %lu byte(s))\n",a,++a,sizeof(++a));
+}
+
