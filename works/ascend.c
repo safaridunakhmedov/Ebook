e92ab3257a237b4583d6c8110e7972791820510f
user <faridun.ahmedov.97@mail.ru>
2019-11-11 14:15:07 +0200

    20191111_14_15

---
+++ works/ascend.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+
+   int a = 10, b=30, c=40;
+
+   if( a < b < c) {
+      printf("%d , %d, %d \n",a,b,c);
+   }
+   return 0;
+}
--- works/oper.c
+++ works/oper.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
+ #include<stdio.h>
 
 void main(){
+#define A 8;
 int a=8, b=9;
 
 printf("%d + %d = %d  (sizeof \"+\" %lu byte(s))\n",a,b,a+b,sizeof(a+b));
@@ -20,10 +21,17 @@ printf("%d >> %d = %d  (sizeof \">>\" %lu byte(s))\n",234,2,234>>2,sizeof(234>>2
 printf("%d += %d = %d  (sizeof \"+=\" %lu byte(s))\n",a,b,a+=b,sizeof(a+=b));
 printf("%d -= %d = %d  (sizeof \"-=\" %lu byte(s))\n",a,b,a-=b,sizeof(a-=b));
 printf("%d *= %d = %d  (sizeof \"*=\" %lu byte(s))\n",a,b,a*=b,sizeof(a*=b));
+a=A;
 printf("%d <<= %d = %d  (sizeof \"<<=\" %lu byte(s))\n",a,b,a<<=b,sizeof(a<<=b));
+a=A
 printf("%d >>= %d = %d  (sizeof \">>=\" %lu byte(s))\n",a,b,a>>=b,sizeof(a>>=b));
-
+a=A;
 printf("%d++ = %d (size of \"a++\" %lu byte(s))\n",a,a++,sizeof(a++));
 printf("++%d = %d (size of \"++a\" %lu byte(s))\n",a,++a,sizeof(++a));
+a=A;
+printf("%d || %d = %d (size of \"||\" %lu byte(s))\n",a,b,(a == b),sizeof(a||b));
+printf("%d ^ %d = %d  (sizeof \"^\" %lu byte(s))\n",a,b,a^b,sizeof(a^b));
+printf("%d = %d(size of \"~a\" %lu byte(s))\n",a,~a,sizeof(a++));
 }
+
 
